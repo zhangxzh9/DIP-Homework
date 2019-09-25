9cdcf51ac5717cd85d0c52d8bea355ebb9a54f27
zhangxz <838247653@qq.com>
2019-09-25 12:02:05 +0800

    “change”

--- hw1.py
+++ hw1.py
@@ -5,16 +5,15 @@ from numpy.linalg import solve
 import os,sys 
 def scale(image,size):#size[0]:highth size[1]: width
     I_array = np.array(image)
-    np.savetxt("image.txt",I_array)
+    #np.savetxt("image.txt",I_array)
     tar_width , tar_height = size
     src_height , src_width = I_array.shape[0:2]
-    
-
+    #calculate the scale
     height_scale = src_height / tar_height
     width_scale = src_width / tar_width
-
+    #creat new picture array
     scaled_array = np.zeros((tar_height,tar_width), np.uint8)
-    print(scaled_array.shape)
+    #print(scaled_array.shape)
     for h in range(tar_height):
         for w in range(tar_width):
             #float source localation
@@ -35,40 +34,26 @@ def scale(image,size):#size[0]:highth size[1]: width
                 f_up = (x_1 - x) * I_array[y_1, x_0] + (x - x_0) * I_array[y_1, x_1]
                 f_down = (x_1 - x) * I_array[y_0, x_0] + (x - x_0) * I_array[y_0, x_1]
                 scaled_array[h,w] = int((y_1 - y) * f_down + (y - y_0) * f_up) 
-
-    np.savetxt("scaled.txt",scaled_array)           
+    #np.savetxt("scaled.txt",scaled_array)           
     return Image.fromarray(scaled_array)      
-
-    # if src_width > tar_width and src_height > tar_height: #down scale
-    #     for w in range(tar_width):
-    #         for h in range(tar_height):
-    #             corr_x = (w+0.5)/h*pic.shape[0]-0.5
-    #             corr_y = (j+0.5)/w*pic.shape[1]-0.5
-    # elif src_width < tar_width and src_height < tar_height : #up scale
-    #     for width in range(size[0]):
-    #         for heigth in range(size[1]):
-    #             pass
-    # else: # scale
-    #     for width in range(size[0]):
-    #         for heigth in range(size[1]):
-    #             pass
-    #return scaled_image
-
+def quantize(image,level):
+    level = level if (level >= 1 and level <= 256) else ( 1 if level < 1 else 256)
+    
 
 if __name__ == "__main__":
     I = Image.open('38.png') 
     #I.show()    
-    Image_scaled = scale(I,(192,128))
-    Image_scaled.save('./scaled_192_168.png')
-    Image_scaled = scale(I,(96,64))
-    Image_scaled.save('./scaled_96_64.png')
-    Image_scaled = scale(I,(48,32))
-    Image_scaled.save('./scaled_48_32.png')
-    Image_scaled = scale(I,(24,16))
-    Image_scaled.save('./scaled_24_16.png')
-    Image_scaled = scale(I,(300,200))
-    Image_scaled.save('./scaled_300_200.png')
-    Image_scaled = scale(I,(450,300))
-    Image_scaled.save('./scaled_450_300.png')
-    Image_scaled = scale(I,(500,200))
-    Image_scaled.save('./scaled_500_200.png')
+    # Image_scaled = scale(I,(192,128))
+    # Image_scaled.save('./scaled_192_168.png')
+    # Image_scaled = scale(I,(96,64))
+    # Image_scaled.save('./scaled_96_64.png')
+    # Image_scaled = scale(I,(48,32))
+    # Image_scaled.save('./scaled_48_32.png')
+    # Image_scaled = scale(I,(24,16))
+    # Image_scaled.save('./scaled_24_16.png')
+    # Image_scaled = scale(I,(300,200))
+    # Image_scaled.save('./scaled_300_200.png')
+    # Image_scaled = scale(I,(450,300))
+    # Image_scaled.save('./scaled_450_300.png')
+    # Image_scaled = scale(I,(500,200))
+    # Image_scaled.save('./scaled_500_200.png')
